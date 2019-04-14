352ed055b971941051957fc1bb996c6a9573e0d4
Allister Levi Sanchez <allister.sanchez@amadeus.com>
2019-04-14 21:34:41 +0200

    First working Hello World

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cpp-web-app
 A web application using C++
+
+To build:
+
+```bash
+$ docker run -it -v "$(pwd)/src":/src allister/cpp-web-dev:1.0.0 bash
+$ mkdir -p /build
+$ cd /build
+$ cmake /src
+$ cmake --build .
+```
+
+To run:
+
+```bash
+$ /build/cpp-web-app
+```
---
+++ src/cpp-web-app.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cpp-web-app.cpp
+// Author      : Allister Sanchez
+// Version     :
+// Copyright   : Copyright Allister Sanchez 2019
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
