deb135e577ac743eafab6dbb158573427c1d9633
Allister Levi Sanchez <allister.sanchez@amadeus.com>
2019-04-19 19:12:01 +0200

    Implemented first http server with simple hello world

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM allister/cpp-web-dev:1.0.0 as builder
-COPY src/* /src/
+FROM allister/cpp-web-dev:latest as builder
+COPY src/ /src/
 WORKDIR /build
 RUN cmake /src && cmake --build .
 
-FROM ubuntu:18.04
+FROM allister/cpp-web-dev:lean-1.0.0
 WORKDIR /app
 COPY --from=builder /build/cpp-web-app .
 CMD ["./cpp-web-app"]
--- src/cpp-web-app.cpp
+++ src/cpp-web-app.cpp
@@ -3,13 +3,18 @@
 // Author      : Allister Sanchez
 // Version     :
 // Copyright   : Copyright Allister Sanchez 2019
-// Description : Hello World in C++, Ansi-style
+// Description : A web application written in C++, based on feather.
 //============================================================================
 
-#include <iostream>
-using namespace std;
+#include <cinatra.hpp>
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+  int maximum_thread_number = std::thread::hardware_concurrency();
+  cinatra::http_server server(maximum_thread_number);
+  server.listen("0.0.0.0", "3000");
+  server.set_http_handler<cinatra::GET>("/", [](cinatra::request& request, cinatra::response& response) {
+    response.set_status_and_content(cinatra::status_type::ok, "hello world");
+  });
+  server.run();
+  return 0;
 }
